baabb18ed8c6880ac72e0e33b9ead90baba172ca
zhangxin <1130018964@qq.com>
2020-04-23 18:35:05 +0800

    002  两数相加

--- golang/002/addTwoNumbers.go
+++ golang/002/addTwoNumbers.go
@@ -1,17 +1,43 @@
 package AddTwoNumbers
 
 type ListNode struct {
+	Val  int
+	Next *ListNode
 }
 
 /**
  * Definition for singly-linked list.
+ * 使用递归的思路来解答
  * type ListNode struct {
  *     Val int
  *     Next *ListNode
  * }
  */
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	result := &ListNode{}
-
-	return result
+	if l1 == nil && l2 == nil {
+		return nil
+	}
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	sum := l1.Val + l2.Val
+	nextNode := addTwoNumbers(l1.Next, l2.Next)
+	if sum < 10 {
+		return &ListNode{
+			Val:  sum,
+			Next: nextNode,
+		}
+	} else {
+		tempNode := &ListNode{
+			Val:  1,
+			Next: nil,
+		}
+		return &ListNode{
+			Val:  sum - 10,
+			Next: addTwoNumbers(nextNode, tempNode),
+		}
+	}
 }

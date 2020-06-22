dd780c917206d4990e084a1dc23a9d180f302953
iris <1130018964@qq.com>
2020-06-22 21:02:19 +0800

    005 动态规划

---
+++ php/005/longestPalindrome.php
@@ -0,0 +1,13 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param String $s
+     * @return String
+     */
+    function longestPalindrome($s) {
+        //动态规划  定义字符串i 到 j 之间回文 则 i+1 到j-1之间肯定回文
+        
+    }
+}

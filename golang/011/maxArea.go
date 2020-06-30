e4b2f83de72c0bac9edea72b15cab0cad959d096
iris <1130018964@qq.com>
2020-06-30 19:22:11 +0800

    011 盛最多水的容器

---
+++ golang/011/maxArea.go
@@ -0,0 +1,24 @@
+package 011
+
+func maxArea(height []int) int {
+	i :=0
+	j := len(height) - 1
+	res := 0
+	for i < j {
+		if height[i] < height[j] {
+			res = max(res,height[i] * (j - i))
+			i += 1
+		} else {
+			res = max(res,height[j] * (j-i))
+			j -= 1
+		}
+	}
+	return res
+}
+
+func max(x, y int) int {
+	if x > y {
+	  return x
+	}
+	return y
+   }

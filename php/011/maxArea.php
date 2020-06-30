606db636f0140b09415c3629f1fb7068efd0f21f
iris <1130018964@qq.com>
2020-06-30 17:51:29 +0800

    011 盛最多水的容器

---
+++ php/011/maxArea.php
@@ -0,0 +1,38 @@
+<?php
+
+class Solution
+{
+    /**
+     * @param Integer[] $height
+     * @return Integer
+     */
+    public function maxArea($height)
+    {
+        // $count = count($height);
+        // $result = [];
+        // for ($i = 0; $i < $count; $i++) {
+        //     for ($j = $i; $j < $count; $j++) {
+        //         $h = min($height[$j],$height[$i]);
+        //         $w = $j - $i;
+        //         $result[] = $w * $h;
+        //     }
+        // }
+
+        // return max($result);
+
+        $i = 0;
+        $j = count($height) - 1;
+        $res =0;
+        while ($i < $j) {
+            if ($height[$i] < $height[$j]) {
+                $res = max($res,$height[$i] * ($j-$i));
+                $i += 1;
+            } else {
+                $res = max($res,$height[$j] * ($j - $i));
+                $j -= 1;
+            }
+        }
+
+        return $res;
+    }
+}
